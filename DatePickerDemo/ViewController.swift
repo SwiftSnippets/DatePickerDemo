f245029aea7f80015e4552d9d00fcc6f207f430d
Abdulsalam Mansour <abdulsalam.mans96@gmail.com>
2020-01-13 01:35:14 -0800

    add date picker

--- DatePickerDemo/ViewController.swift
+++ DatePickerDemo/ViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dateTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let datePicker = UIDatePicker()
+        
+        datePicker.datePickerMode = UIDatePicker.Mode.date
+        datePicker.addTarget(self, action: #selector (ViewController.datePickerValueChanged(sender:)), for: UIControl.Event.valueChanged)
+        //the following line shows date picker instead of keyboard
+        dateTextField.inputView = datePicker
+    }
+    
+    @objc func datePickerValueChanged(sender : UIDatePicker){
+        let formatter = DateFormatter()
+        
+        formatter.dateStyle = DateFormatter.Style.medium
+        formatter.timeStyle = DateFormatter.Style.none
+        dateTextField.text! = formatter.string(from: sender.date)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
     }
 
 
